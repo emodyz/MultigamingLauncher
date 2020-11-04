e1562b2f84d8adade2cdbcb30ed2d2de71030f08
Leo Hubert <leo.hubert@epitech.eu>
2020-11-04 14:00:55 +0100

    Add server view

---
+++ src/renderer/pages/servers/_id.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex justify-center items-center content-center">
+      <h1> {{server.name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const id = params.id
+    return { id };
+  },
+
+  data() {
+    return {
+      server: null,
+    }
+  },
+
+  mounted() {
+    this.loadServer();
+  },
+
+  methods: {
+    async loadServer() {
+      try {
+        this.server = (await this.$axios.$get(`/servers/${this.id}`)).data;
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/renderer/pages/servers/index.vue
+++ src/renderer/pages/servers/index.vue
@@ -1,12 +1,42 @@
 <template>
-  <div>
-    SERVER LIST
+  <div class="flex justify-center">
+    <div v-for="server in servers" class="flex flex-col items-center m-4 w-1/3">
+      <img :src="server.game.logo_url" class="rounded w-20">
+      <span>{{server.name}}</span>
+
+      <NuxtLink :to="'/servers/' + server.id">
+        <button class="bg-blue hover:bg-blue text-blue-dark font-semibold hover:text-blue py-2 px-4 border border-blue hover:border-transparent rounded">
+          PLAY
+        </button>
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "index"
+  name: "index",
+
+  data() {
+    return {
+      servers: []
+    }
+  },
+
+  mounted() {
+    this.loadServers();
+  },
+
+  methods: {
+    async loadServers() {
+      try {
+        this.servers = (await this.$axios.$get('/servers')).data;
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+
 }
 </script>
 
