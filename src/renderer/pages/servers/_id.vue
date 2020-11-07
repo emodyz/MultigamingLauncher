cf4d68067c45b9a91012261bf6c9ef9eddcda8a8
Leo Hubert <leo.hubert@epitech.eu>
2020-11-07 03:25:36 +0100

    Add downloader

--- src/renderer/pages/servers/_id.vue
+++ src/renderer/pages/servers/_id.vue
@@ -17,21 +17,34 @@
       </div>
     </div>
 
-
-    <div class="relative pt-1">
-      <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-teal-200">
-        <div :style="'width: ' + progress + '%'" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-500"></div>
+    <div v-if="downloading">
+      <div class="flex flex-col items-center justify-center content-center">
+        <span>{{ speed }}</span>
+        <span v-if="this.downloader">{{ this.downloader.filesDownloaded }} / {{ this.downloader.filesToDownload }}</span>
+      </div>
+      <div class="relative pt-1">
+        <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-red-200">
+          <div :style="'width: ' + totalProgress + '%'" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500 progress-animation" />
+        </div>
+      </div>
+      <div class="relative pt-1">
+        <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-teal-200">
+          <div :style="'width: ' + progress + '%'" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-500 progress-animation" />
+        </div>
+      </div>
     </div>
-  </div>
 
-    <div class="flex items-center flex-col justify-center shadow-lg absolute bottom-0 left-0 right-0 h-20">
-      <button class="bg-blue-500 text-gray-300 rounded px-4 py-2" @click="startDownload">DOWNLOAD</button>
+    <div class="flex items-center flex-row justify-center shadow-lg absolute bottom-0 left-0 right-0 h-20">
+      <button v-if="progress" class="m-2 bg-blue-500 text-gray-300 rounded px-4 py-2" @click="stopDownload">STOP</button>
+      <button v-if="progress" class="m-2 bg-blue-500 text-gray-300 rounded px-4 py-2" @click="pauseDownload">PAUSE</button>
+      <button v-if="progress" class="m-2 bg-blue-500 text-gray-300 rounded px-4 py-2" @click="resumeDownload">RESUME</button>
+      <button :disabled="downloading" class=" m-2 bg-blue-500 text-gray-300 rounded px-4 py-2" @click="startDownload">DOWNLOAD</button>
     </div>
   </div>
 </template>
 
 <script>
-import {DownloaderHelper} from 'node-downloader-helper';
+import {createDownloader} from '@emodyz/node-downloader'
 
 export default {
   transition: 'fade',
@@ -43,6 +56,10 @@ export default {
 
   data () {
     return {
+      downloader: null,
+      downloading: false,
+      speed: 0,
+      totalProgress: 0,
       progress: 0,
       checkServerInterval: null,
       server: null
@@ -58,6 +75,7 @@ export default {
 
   beforeDestroy () {
     clearInterval(this.checkServerInterval)
+    this.stopDownload()
   },
 
   methods: {
@@ -69,32 +87,88 @@ export default {
       }
     },
 
+    humanFileSize (bytes, si = false, dp = 1) {
+      const thresh = si ? 1000 : 1024
+
+      if (Math.abs(bytes) < thresh) {
+        return bytes + ' B'
+      }
+
+      const units = si
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+      let u = -1
+      const r = 10 ** dp
+
+      do {
+        bytes /= thresh
+        ++u
+      } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1)
+
+      return bytes.toFixed(dp) + ' ' + units[u]
+    },
+
     startDownload () {
-      this.progress = 0;
+      this.downloading = true
+      this.totalProgress = 0
+      this.progress = 0
       this.$axios.$get(`/servers/${this.id}/modpacks`)
         .then(res => res.data)
         .then(modpacks => {
-          modpacks.forEach(modpack => {
-            const manifest = modpack.manifest
+          this.downloader = createDownloader({ httpsRequestOptions: { rejectUnauthorized: false } })
+          this.downloader.simultaneusDownloads = 1
 
-            Object.values(manifest).forEach(file => {
-              console.log(file)
-              const dl = new DownloaderHelper(file.url, '/Users/hubert_i/Downloads/test', {httpsRequestOptions: {rejectUnauthorized: false}});
-              dl.on('end', () => console.log('Download Completed'))
-              dl.on('progress', stats => {
-                this.progress = stats.progress;
-              });
-              dl.start();
+          modpacks.forEach(modpack => {
+            const manifest = Object.values(modpack.manifest)
+
+            manifest.forEach(file => {
+              this.downloader.addFile(file.url, '/Users/hubert_i/Downloads/test')
             })
           })
-        }).catch(e => console.log(e))
-    }
 
+          this.downloader.on('progress.total', stats => {
+            this.totalProgress = stats.progress
+          })
+
+          this.downloader.on('progress', stats => {
+            this.progress = stats.progress
+            this.speed = this.humanFileSize(stats.speed) + '/s'
+            console.log(this.progress)
+          })
+
+          this.downloader.on('end', stats => {
+            this.downloading = false
+            this.totalProgress = 0
+            this.progress = 0
+          })
+
+          this.downloader.start()
+        }).catch(e => console.log(e))
+    },
+
+    pauseDownload () {
+      this.downloader.pause()
+    },
+
+    resumeDownload () {
+      this.downloader.resume()
+    },
+
+    stopDownload () {
+      this.downloader.stop()
+      this.downloading = false
+      this.totalProgress = 0
+      this.progress = 0
+    }
   }
 
 }
 </script>
 
 <style scoped>
+
+.progress-animation {
+  transition: width 200ms;
+}
 
 </style>
