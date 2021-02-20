e6b7d5254ce91843307a12f845976787f23606a1
Leo Hubert <leo.hubert@epitech.eu>
2021-02-20 11:50:34 +0100

    Upgrade Nuxt.JS

--- src/renderer/tailwind.config.js
+++ src/renderer/tailwind.config.js
@@ -9,14 +9,11 @@ module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
     content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js',
-      // TypeScript
-      'plugins/**/*.ts',
-      'nuxt.config.ts'
+      './components/**/*.{vue,js}',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './plugins/**/*.{js,ts}',
+      './nuxt.config.{js,ts}'
     ],
     options: {
       safelist: [
